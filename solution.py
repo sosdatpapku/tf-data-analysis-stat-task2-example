1e9fcdfdaa2ab6a1998ee714c569a17372ed0dd8
sosdatpapku <114177169+sosdatpapku@users.noreply.github.com>
2023-04-05 23:32:36 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,7 +9,7 @@ chat_id = 1188007817 # Ваш chat ID, не меняйте название пе
 def solution(p: float, x: np.array) -> tuple:
     n = len(x)
     alpha = 1 - p
-    chi_sq = np.sum(x**2, axis=1) / (37 * np.var(x))
+    chi_sq = np.sum(x**2) / (37 * np.var(x))
     chi_left = chi2.ppf(q=alpha/2, df=1)
     chi_right = chi2.ppf(q=1-alpha/2, df=1)
     sigma_left = np.sqrt(np.sum(x**2) / (37 * chi_right))
